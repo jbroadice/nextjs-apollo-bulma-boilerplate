499707c16a572aafb71f05a7c2e13cc1a9e94d5b
Jack Chapple <jchapple@gmail.com>
2018-10-08 14:51:10 +0100

    Remove redundant import.

--- hocs/appWithAuth.js
+++ hocs/appWithAuth.js
@@ -2,8 +2,6 @@ import React from 'react'
 import redirect from '../utils/redirect'
 import checkLoggedIn from '../utils/checkLoggedIn'
 
-import Button from 'react-bulma-components/src/components/button'
-
 export default App => {
   return class WithAuth extends React.Component {
     static displayName = `WithAuth(${App.displayName})`
