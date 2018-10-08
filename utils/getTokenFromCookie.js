68e88d7fd6bb8f1cefdff57b7d1b4860715831a3
Jack Chapple <jchapple@gmail.com>
2018-10-08 15:01:36 +0100

    Catch no cookies case.

--- utils/getTokenFromCookie.js
+++ utils/getTokenFromCookie.js
@@ -2,6 +2,10 @@ import cookie from 'cookie'
 import isNode from './isNode'
 
 export default function getTokenFromCookie(headers) {
+  if (!headers.cookie) {
+    return null
+  }
+
   const cookies = cookie.parse(isNode() ? headers.cookie : document.cookie)
   return cookies.token
 }
