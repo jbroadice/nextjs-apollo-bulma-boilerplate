24b0124f8e93624a3ace269702b4ffab6c7ec959
Jack Chapple <jchapple@gmail.com>
2018-10-15 13:35:00 +0100

    Remove console.log statement

--- utils/getTokenFromCookie.js
+++ utils/getTokenFromCookie.js
@@ -3,8 +3,6 @@ import cookie from 'cookie'
 export default function getTokenFromCookie(headers) {
   const cookies = process.browser ? document.cookie : headers.cookie
 
-  console.log('getTokenFromCookie', cookies)
-
   if (cookies) {
     return cookie.parse(cookies).token
   }
